36bdc21b2cc6cd1c8e26d52d55c452c68300d5e3
Joey Kok Yen Ni <92930651+yenny1o15@users.noreply.github.com>
2023-09-03 22:52:23 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c2fmaqmh4zo5.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "joeykokyenni-employee"
+customregion = "us-east-1"
+
